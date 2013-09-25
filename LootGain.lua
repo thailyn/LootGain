04e6ecd48308955f6c83f651a21561dc412bce47
Charles Macanka <cmacanka@gmail.com>
2013-09-25 11:40:51 -0400

    Added functions to get static and variable player info.
    The GetStaticPlayerInfo function looks up information that does not change
    while a user is logged in, such as his name and class.  The
    GetVariablePlayerInfo function looks up all information which could change
    (but is not directly related to looting), such as the player's
    specializations, level, quests, and inventory.  Both of these functions store
    the collected informatin in the table parameter.

--- LootGain.lua
+++ LootGain.lua
@@ -104,6 +104,94 @@ local function PrintPlayerInfo()
    end
 end
 
+local function GetStaticPlayerInfo(player)
+   player.loginTime = time();
+   player.name = UnitName("player");
+   --player.server = ...;
+   player.class = UnitClass("player");
+   player.race = UnitRace("player");
+end
+
+local function GetVariablePlayerInfo(player)
+   player.guid = UnitGUID("player");
+   player.level = UnitLevel("player");
+   player.inParty = UnitInParty("player");
+   player.inRaid = UnitInRaid("player");
+
+   -- specializations
+   player.numSpecializations = GetNumSpecializations(false; false);
+   player.specializations = { };
+   for i = 1, player.numSpecializations do
+      player.specializations[i] = { };
+
+      local curr = player.specializations[i];
+      curr.id, curr.name, _, curr.icon, _, curr.role = GetSpecializationInfo(i, false, false);
+   end
+   player.activeSpecialization = GetActiveSpecGroup(false);
+   player.currentSpecialization = player.specializations[player.activeSpecialization];
+
+   -- quests
+   local numQuestLogLines, numQuests = GetNumQuestLogEntries();
+   player.numQuests = numQuests;
+   player.quests = { };
+
+   local currentQuestHeader;
+   for i = 1, numQuestLogLines do
+      local questTitle, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, questID = GetQuestLogTitle(i)
+      if (isHeader) then
+         currentQuestHeader = questTitle;
+      else
+         player.quests[questID] = { name = questTitle, level = level, questTag = questTag,
+                                    isDaily = isDaily, id = questID, header = currentQuestHeader };
+      end
+   end
+
+   -- currencies
+   local numCurrencyLines = GetCurrencyListSize();
+   player.currencies = { };
+
+   local numCurrencies = 0;
+   local currentCurrencyHeader;
+   for i = 1, numCurrencyLines do
+      local name, isHeader, isExpanded, isUnused, isWatched, count, icon, maximum,
+      hasWeeklyLimit, currentWeeklyAmount, unknown, itemID = GetCurrencyListInfo(i);
+      if (isHeader) then
+         currentCurrencyHeader = name;
+      else
+         numCurrencies = numCurrencies + 1;
+         player.currencies[i] = { name = name, count = count, weeklyLimit = currentWeeklyAmount,
+                                  itemID = itemID, icon = icon, header = currentCurrencyHeader };
+      end
+   end
+   player.numCurrencies = numCurrencies;
+
+   -- inventory
+   player.inventory = { };
+   local currentBagNum = 0;
+   while GetBagNum(currentBagNum) do
+      local numSlots = GetContainerNumSlots(currentBagNum);
+      for currentSlotNum = 1, numSlots do
+         local itemLink = GetContainerItemLink(currentBagNum, currentSlotNum);
+         local texture, itemCount, locked, quality, readable = GetContainerItemInfo(currentBagNum, currentSlotNum);
+         local isQuestItem, questId, isActive = GetContainerItemQuestInfo(currentBagNum, currentSlotNum);
+
+         player.inventory[itemLink] = player.inventory[itemLink] or { };
+         local curr = player.inventory[itemLink];
+
+         curr.itemLink = itemLink;
+         curr.texture = texture;
+         curr.quality = quality;
+         curr.isQuestItem = isQuestItem;
+         curr.questId = questId;
+         curr.isActive = isActive;
+
+         curr.count = curr.count + itemCount;
+      end
+
+      currentBagNum = currentBagNum + 1;
+   end
+end
+
 function LootGain_OnLoad(self)
    DEFAULT_CHAT_FRAME:AddMessage("Loot Gain 0.01 loaded.");
 
